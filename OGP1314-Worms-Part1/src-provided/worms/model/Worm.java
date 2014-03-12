3c40186d9d3da7c9a5f57c3421570d1bc7d09269
benderT <https://github.com/benderT/wormen.git>
2014-03-12 17:58:14 +0100

    versie 2

--- OGP1314-Worms-Part1/src-provided/worms/model/Worm.java
+++ OGP1314-Worms-Part1/src-provided/worms/model/Worm.java
@@ -9,6 +9,7 @@ private double positionY;
 private int actionPoint;
 private final double minRadius=0.25;
 private static final double Density = 1062.0;
+private static final double Gravity = 9.80665;
 private String name;
 private double radius;
 private double angle;
@@ -130,16 +131,72 @@ public double getOrientation() {
 }
 
 
-public void turn(double angle){
-	assert this.ValidAngle(angle);
-	assert this.ValidAngle(this.getOrientation() + angle);
+public boolean TurnPossible(double angle){
+	int costTurn = (int) Math.ceil(60/(angle/2*Math.PI));
+	if costTurn<= this.getActionPoint()
+		{return true}
+	else {return false;}
+	
 }
 
 
 
+public void turn(double angle){
+	assert this.ValidAngle(angle);
+	assert this.ValidAngle(this.getOrientation() + angle); // weglaten of niet test bij setOrientation of is het een voorwaarde om Turn uit te voeren?
+	assert this.TurnPossible(angle);
+	
+	this.setOrientation(this.getOrientation()+ angle);
+	int costTurn = (int) Math.ceil(60/(angle/2*Math.PI));
+	this.setActionPoint(this.getActionPoint() - costTurn);
+	
+}
+public boolean movePossible(int cost){
+	if (cost<= this.getActionPoint()){
+	return true;}
+}
 
 
 
+public void Move(int nbSteps){
+	int costMove= int Math.ceil(nbSteps*((double)Math.abs(Math.cos(this.getOrientation()))+ (double) Math.abs(4*Math.sin(this.getOrientaion()))))
+	if (movePossible(cost)){
+		this.setActionPount(this.getActionPoint()-costMove)
+		double travelled= nbSteps*this.radius;
+		this.setPositionX(this.getPositionX()+travelled*Math.cos(this.getOrientation()))
+		this.setPositionY(this.getPositionY()+travelled*Math.sin(this.getOrientation()))
+		}
+	else throw IllegalCostExeption(costMove);
+}
+public void Jump(){
+	if(util.fuzzygreatherThanOrEqualTo(this.getOrientation(),0) && !Util.fuzzyGreatherThanOrEqualTo(this.getOrientation(),Math.PI))){
+	double t=this.getJumpTime();
+	double [] endPosition =this.getJumpStep();
+	this.setPositionX(endPosition[0]);
+	this.setPositionY(endPosition[1]);
+	this.setActionPoint(0);
+	}
+}
+
+public double getJumpTime(){
+	double distance= Math.pow(this.getInitialVelocity();2)*Math.sin(2*this.getOrientation()/this.gravity());
+	double t= distance/(this.getInitialVelocity()*Math.cos(this.getOrientation()));
+	return t;
+}
+public double getJumpStep(){
+	double velocityX=this.getInitialVelocity()*Math.cos(this.getOrientation());
+	double velocityY=this.getInitialVelocity()*Math.sin(this.getOrientation());
+	double positionAftherJumpX= (getPositionX() + velocityX*this.getJumpTime);
+	double positionAftherJumpY= (getPositionY() + (velocityY*this.getJumpTime-0.5*(this.gravity()*Math.pow(),2));
+	double endPosition[] = new double[] {positionAftherJumpX,positionAftherJumpY};
+	return endPosition;	
+}
+public double getIntialVelocity() {
+	double F= (5*this.getActionPoint())+(this.getMass()*this.gravity);
+	double V0= ((F/this.getMass())*0.5);
+	return V0;
+		
+}
 
 
 
